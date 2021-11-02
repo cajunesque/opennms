665cad4e0385014d832acdf64f89854ca5eacf76
Dustin Frisch <dustin@opennms.com>
2021-11-02 15:39:13 +0100

    NMS-13652: Subscriber refactoring

--- core/ipc/twin/jms/subscriber/src/main/java/org/opennms/core/ipc/twin/jms/subscriber/JmsTwinSubscriber.java
+++ core/ipc/twin/jms/subscriber/src/main/java/org/opennms/core/ipc/twin/jms/subscriber/JmsTwinSubscriber.java
@@ -88,7 +88,7 @@ public class JmsTwinSubscriber extends AbstractTwinSubscriber implements Process
                     byte[] response = exchange.getOut().getBody(byte[].class);
                     TwinUpdate twinUpdate = mapTwinResponseToProto(response);
                     if (twinUpdate.getLocation() == null ||
-                            twinUpdate.getLocation().equals(getMinionIdentity().getLocation())) {
+                            twinUpdate.getLocation().equals(getIdentity().getLocation())) {
                         LOG.trace("Received TwinUpdate as RPC reply {}", twinUpdate);
                         accept(twinUpdate);
                     }
@@ -129,7 +129,7 @@ public class JmsTwinSubscriber extends AbstractTwinSubscriber implements Process
         byte[] sinkUpdateBytes = exchange.getIn().getBody(byte[].class);
         TwinUpdate twinUpdate = mapTwinResponseToProto(sinkUpdateBytes);
         if (twinUpdate.getLocation() == null ||
-                twinUpdate.getLocation().equals(getMinionIdentity().getLocation())) {
+                twinUpdate.getLocation().equals(getIdentity().getLocation())) {
             LOG.trace("Received TwinResponse as sink update {}", twinUpdate);
             accept(twinUpdate);
         }
